ca6f63a597f5df8fb7be433516a38a59b88b7e37
Oluwademilade Ala <demiladeala@gmail.com>
2024-06-24 16:37:08 +0100

    feat: add setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
+import { Footer } from "./components/footer"
+import { Gallery } from "./components/gallery"
+import { Hero } from "./components/hero"
+import { Navbar } from "./components/navbar"
+import { OurWeek } from "./components/our-week"
+import { Journey } from "./journey"
+import { Sponsor } from "./sponsor"
+import { Vision } from "./vision"
 
 
 function App() {
   return (
     <div className="relative container w-full mx-auto max-w-[2000px]">
-      Navbar
-      Hero
-      Vision
-      Journey
-      Our week
-      Gallery
-      Sponsor
-      Footer
+      <Navbar />
+      <Hero />
+      <Vision />
+      <Journey />
+      <OurWeek />
+      <Gallery />
+      <Sponsor />
+      <Footer />
     </div>
   )
 }

c7b368ca3a5b569b1746a32580e240f527efed67
Oluwademilade Ala <demiladeala@gmail.com>
2024-06-24 23:29:45 +0100

    feat: add our-week

--- src/components/our-week.tsx
+++ src/components/our-week.tsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 export const OurWeek = () => {
     return (
-        <div>
-            OurWeek
+        <div className='w-[95%] mx-auto md:w-[90%] mt-14 lg:mt-28'>
+            <h2 className='font-extrabold text-3xl md:text-4xl text-center'>Our Week at a Glance</h2>
+            <p className='text-lg md:text-xl text-center mt-5'>
+            Explore our schedule of events and activities for the week, packed with 
+            learning, collaboration, and memorable moments.
+            </p>
         </div>
     )
 }
--- src/vision.tsx
+++ src/vision.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const Vision = () => {
     return (
-        <div className='w-[95%] md:w-[90%] mx-auto flex flex-col gap-9 lg:gap-16 xl:gap-28 lg:flex-row items-center py-14'>
-            <h2 className='font-extrabold text-4xl'>Our Vision</h2>
+        <div className='w-[95%] md:w-[90%] mx-auto flex flex-col gap-9 lg:gap-16 xl:gap-28 lg:flex-row items-center py-9 md:py-14'>
+            <h2 className='font-extrabold text-3xl md:text-4xl'>Our Vision</h2>
 
             <p className='text-xl lg:w-[80%] xl:w-[75%] max-lg:text-center'>We envision a future where our collective efforts drive 
                 technological advancements and create opportunities for 

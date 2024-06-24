17f32f8387a6a94a17d8dfb32c21456546773a10
Oluwademilade Ala <demiladeala@gmail.com>
2024-06-24 19:42:57 +0100

    feat: add vision

--- src/journey.tsx
+++ src/journey.tsx
@@ -2,8 +2,38 @@ import React from 'react'
 
 export const Journey = () => {
     return (
-        <div>
-            Journey
+        <div className='text-white grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-14'>
+            <div className='bg-[#000406] py-12 px-5 md:px-14'>
+                <h3 className='font-extrabold text-4xl max-lg:text-center'>Our Journey</h3>
+
+                <div className='mt-5 py-3 border-b border-white/40'>
+                    <p className='text-xl'>2018: Freshers&apos; Orientation</p>
+                </div>
+
+                <div className='mt-5 py-3 border-b border-white/40'>
+                    <p className='text-xl'>2019: First Year Projects and Hackathons</p>
+                </div>
+
+                <div className='mt-5 py-3 border-b border-white/40'>
+                    <p className='text-xl'>2020: Online Learning Transition</p>
+                </div>
+
+                <div className='mt-5 py-3 border-b border-white/40'>
+                    <p className='text-xl'>2021: Internship Experiences</p>
+                </div>
+
+                <div className='mt-5 py-3 border-b border-white/40'>
+                    <p className='text-xl'>2022: Major Projects and Research Contributions</p>
+                </div>
+
+                <div className='mt-5 py-3 border-b border-white/40'>
+                    <p className='text-xl'>2023: Final Year Projects and Industry Collaborations</p>
+                </div>
+
+                <div className='mt-5 py-3 border-b border-white/40'>
+                    <p className='text-xl'>2024: Graduation Ceremony</p>
+                </div>
+            </div>
         </div>
     )
 }
--- src/vision.tsx
+++ src/vision.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 export const Vision = () => {
     return (
-        <div className='w-[95%] mx-auto flex flex-col gap-9 lg:gap-16 md:flex-row items-center py-12 lg:py-14'>
-            <h2 className='font-extrabold text-3xl md:w-[10%]'>Our Vision</h2>
+        <div className='w-[95%] md:w-[90%] mx-auto flex flex-col gap-9 lg:gap-16 xl:gap-28 md:flex-row items-center py-14'>
+            <h2 className='font-extrabold text-4xl'>Our Vision</h2>
 
-            <p className='text-xl md:w-[80%]'>We envision a future where our collective efforts drive 
+            <p className='text-xl md:w-[80%] xl:w-[75%]'>We envision a future where our collective efforts drive 
                 technological advancements and create opportunities for 
                 the next generation of innovators.</p>
         </div>

aca5325cf8785b1f39474729db3c62e814199cc9
Oluwademilade Ala <demiladeala@gmail.com>
2024-06-25 00:40:39 +0100

    feat: add sponsor

--- src/sponsor.tsx
+++ src/sponsor.tsx
@@ -9,16 +9,15 @@ export const Sponsor = () => {
                 </div>
 
                 <div className='z-[2] relative flex flex-col lg:flex-row gap-5 lg:gap-10 items-center lg:justify-between'>
-                    <div className='text-white' >
+                    <div className='text-white lg:basis-[60%]' >
                         <h2 className='font-extrabold text-3xl md:text-4xl max-lg:text-center'>Do you want to sponsor with us?</h2>
 
                         <p className='text-xl max-lg:text-center mt-4'>
-                        We envision a future where our collective efforts drive 
                         Partner with us and help shape the future of technology. Your support can make a significant impact</p>
                     </div>
 
 
-                    <div className='w-full max-lg:mt-5 flex items-center justify-center'>
+                    <div className='max-lg:w-full max-lg:mt-5 flex items-center justify-center'>
                     <button className='max-lg:w-full bg-primary-blue text-white py-4 px-5 lg:py-5 lg:px-28 rounded-3xl transition duration-500 hover:bg-primary-purple'>Get in touch</button>
                     </div>
                 </div>

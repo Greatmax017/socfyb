d6726a0fd8fdabfb8c53dec994ac974e23b24428
Oluwademilade Ala <demiladeala@gmail.com>
2024-06-25 00:51:17 +0100

    feat: add footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,8 +2,52 @@ import React from 'react'
 
 export const Footer = () => {
     return (
-        <div>
-            Footer
+        <div className='mt-8 bg-primary-darkBlue text-white'>
+            <div className='w-[95%] md:w-[90%] mx-auto py-8 md:py-12 lg:py-16'>
+                <div className='flex flex-col gap-6 lg:flex-row lg:items-center lg:justify-between'>
+                    <div className=''>
+                        <img 
+                        src="/inverted-logo.svg" 
+                        alt="logo"
+                        width={215}
+                        height={55}
+                        className='max-[350px]:w-32 max-sm:w-40 object-cover aspect-auto' />
+                        <p className="mt-2 max-md:text-sm">Empowering future tech leaders through innovation and collaboration</p>
+                    </div>
+
+                    <div className="flex flex-col gap-6 lg:gap-16 lg:flex-row lg:items-start">
+                        <div className="flex flex-col gap-3 md:text-xl">
+                            <h3 className="font-extrabold md:text-xl">Quick Link</h3>
+                            <a href="">
+                                <p>About</p>
+                            </a>
+
+                            <a href="">
+                                <p>Our Journey</p>
+                            </a>
+
+                            <a href="">
+                                <p>Gallery</p>
+                            </a>
+                        </div>
+
+                        <div className="flex flex-col gap-3 md:text-xl">
+                            <h3 className="font-extrabold md:text-xl">Support</h3>
+                            <a href="">
+                                <p>Contact Us</p>
+                            </a>
+
+                            <a href="">
+                                <p>Events</p>
+                            </a>
+
+                            <a href="">
+                                <p>Become a Sponsor</p>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }

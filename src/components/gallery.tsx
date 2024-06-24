dde5022a1d3ad205a55552ffd6ea5fc4570bc68a
Oluwademilade Ala <demiladeala@gmail.com>
2024-06-24 23:58:53 +0100

    feat: add gallery

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { IoMdArrowBack, IoMdArrowForward } from 'react-icons/io'
 
 export const Gallery = () => {
     return (
-        <div>
-            Gallery
+        <div className='bg-[#202046] mt-4'>
+            <div className='w-[95%] md:w-[90%] mx-auto flex flex-col lg:flex-row items-end justify-between text-white py-14'>
+                <div className='w-full'>
+                    <h2 className='font-extrabold text-3xl md:text-4xl'>Gallery</h2>
+                    <p className='text-lg md:text-xl mt-1'>
+                    A curated selection of photos from various moments over the last couple of years
+                    </p>
+                </div>
+
+                <div className='max-lg:w-full max-lg:flex max-lg:justify-end max-lg:mt-4 flex items-center gap-3'>
+                    <div className='p-3 rounded-full border border-primary-blue transition duration-500 hover:bg-primary-blue'>
+                        <IoMdArrowBack size={25}/>
+                    </div>
+
+                    <div className='p-3 rounded-full border border-primary-blue transition duration-500 hover:bg-primary-blue'>
+                        <IoMdArrowForward size={25}/>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/our-week.tsx
+++ src/components/our-week.tsx
@@ -3,7 +3,7 @@ import { OurWeekCard } from './our-week-card'
 
 export const OurWeek = () => {
     return (
-        <div className='w-[95%] mx-auto md:w-[90%] mt-14 lg:mt-28'>
+        <div className='w-[95%] mx-auto md:w-[90%] mt-14 lg:mt-28 mb-20'>
             <h2 className='font-extrabold text-3xl md:text-4xl text-center'>Our Week at a Glance</h2>
             <p className='text-lg md:text-xl text-center mt-5'>
             Explore our schedule of events and activities for the week, packed with 

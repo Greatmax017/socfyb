28a8464a517e0f2347e238ef820635d30594a35b
Oluwademilade Ala <demiladeala@gmail.com>
2024-06-24 23:24:04 +0100

    feat: add journey

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,36 +3,38 @@ import { HiMenuAlt3 } from 'react-icons/hi'
 
 export const Navbar = () => {
     return (
-        <nav className='fixed top-0 left-0 w-full bg-white py-3 px-5 md:px-6 lg:px-8 flex items-center justify-between shadow-md lg:py-4'>
-            <img 
-            src="/logo.svg" 
-            alt="logo"
-            width={215}
-            height={55}
-            className='max-[350px]:w-32 max-sm:w-40 object-cover aspect-auto' />
+        <div className='fixed top-0 left-0 w-full bg-white shadow-md '>
+            <nav className='w-[95%] mx-auto md:w-[90%] py-3 flex items-center justify-between lg:py-4'>
+                <img 
+                src="/logo.svg" 
+                alt="logo"
+                width={215}
+                height={55}
+                className='max-[350px]:w-32 max-sm:w-40 object-cover aspect-auto' />
 
-            <div className='max-lg:hidden flex items-center gap-7 text-primary-black-100 capitalize'>
-                <a href="" className='nav-links relative hover:font-semibold'>
-                    <p>Home</p>
-                </a>
+                <div className='max-lg:hidden flex items-center gap-7 text-primary-black-100 capitalize'>
+                    <a href="" className='nav-links relative hover:font-semibold'>
+                        <p>Home</p>
+                    </a>
 
-                <a href="" className='nav-links relative hover:font-semibold'>
-                    <p>Our Journey</p>
-                </a>
+                    <a href="" className='nav-links relative hover:font-semibold'>
+                        <p>Our Journey</p>
+                    </a>
 
-                <a href="" className='nav-links relative hover:font-semibold'>
-                    <p>Events</p>
-                </a>
+                    <a href="" className='nav-links relative hover:font-semibold'>
+                        <p>Events</p>
+                    </a>
 
-                <a href="" className='nav-links relative hover:font-semibold'>
-                    <p>Gallery</p>
-                </a>
-            </div>
+                    <a href="" className='nav-links relative hover:font-semibold'>
+                        <p>Gallery</p>
+                    </a>
+                </div>
 
-            <div className='flex items-center gap-2'>
-                <button className='bg-primary-blue text-white py-2 lg:py-3 px-5 lg:px-7 rounded-3xl transition duration-500 hover:bg-primary-purple'>Payments</button>
-                <HiMenuAlt3 size={25} className='lg:hidden'/>
-            </div>
-        </nav>
+                <div className='flex items-center gap-2'>
+                    <button className='bg-primary-blue text-white py-2 lg:py-3 px-5 lg:px-7 rounded-3xl transition duration-500 hover:bg-primary-purple'>Payments</button>
+                    <HiMenuAlt3 size={25} className='lg:hidden'/>
+                </div>
+            </nav>
+        </div>
     )
 }
---
+++ src/journey.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react'
+import { FaPlay } from 'react-icons/fa';
+
+export const Journey = () => {
+    const [showVideo, setShowVideo] = useState(false);
+
+  const handleClick = () => {
+    setShowVideo(true);
+  }
+
+    return (
+        <div className='text-white flex flex-col lg:flex-row gap-8 lg:gap-14'>
+            <div className='w-full lg:basis-[50%] bg-[#000406] py-12 px-5 md:px-14 xl:px-16'>
+                <h3 className='font-extrabold text-4xl max-lg:text-center'>Our Journey</h3>
+
+                <div className='mt-5 py-3 border-b border-white/40'>
+                    <p className='text-xl'>2018: Freshers&apos; Orientation</p>
+                </div>
+
+                <div className='mt-5 py-3 border-b border-white/40'>
+                    <p className='text-xl'>2019: First Year Projects and Hackathons</p>
+                </div>
+
+                <div className='mt-5 py-3 border-b border-white/40'>
+                    <p className='text-xl'>2020: Online Learning Transition</p>
+                </div>
+
+                <div className='mt-5 py-3 border-b border-white/40'>
+                    <p className='text-xl'>2021: Internship Experiences</p>
+                </div>
+
+                <div className='mt-5 py-3 border-b border-white/40'>
+                    <p className='text-xl'>2022: Major Projects and Research Contributions</p>
+                </div>
+
+                <div className='mt-5 py-3 border-b border-white/40'>
+                    <p className='text-xl'>2023: Final Year Projects and Industry Collaborations</p>
+                </div>
+
+                <div className='mt-5 py-3 border-b border-white/40'>
+                    <p className='text-xl'>2024: Graduation Ceremony</p>
+                </div>
+            </div>
+
+            <div className='w-full lg:basis-[50%] relative lg:flex lg:items-center lg:mr-[3%]'>
+                <div className='w-[93%] h-[300px] md:h-[500px] lg: mx-auto relative'>
+                    {!showVideo && (
+                        <>
+                            <div className='z-[6] absolute inset-0 bg-[#0000004D] flex items-center justify-center rounded'>
+                                <FaPlay
+                                onClick={handleClick} 
+                                size={50} 
+                                className='max-md:w-20 cursor-pointer'
+                                />
+                            </div>
+                            <img src="/journey.png" alt='how-we-work' className='w-full h-full object-cover rounded' />
+                        </>
+                        )}
+
+                        {showVideo && (
+                        <video controls autoPlay className='w-full h-[300px] md:h-[500px] object-cover rounded'>
+                        <source src="journey.mp4" type="video/mp4"/>
+                        Your browser does not support the video tag.
+                        </video>
+                    )}
+                </div>
+            </div>
+        </div>
+    )
+}
